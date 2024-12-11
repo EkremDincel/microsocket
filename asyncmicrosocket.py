862150b579441301633dc0bfba14e624427900da
EkremDincel <ekremdincel@gmail.com>
2024-12-11 15:40:08 +0300

    uvx ruff format

--- asyncmicrosocket.py
+++ asyncmicrosocket.py
@@ -1,101 +1,101 @@
 import socket
+
 try:
-    from . import microsocket
+	from . import microsocket
 except ImportError:
-    import microsocket
+	import microsocket
 
 import asyncio
+
 YIELD_TO_LOOP = lambda: asyncio.sleep(0)
 
 __all__ = ["AsyncServer", "AsyncClient", "YIELD_TO_LOOP"]
 
-class AsyncBaseSocket(microsocket.BaseSocket):
 
-    def __init__(self):
-        self.socket = socket.socket()
-        self.socket.setblocking(False)
+class AsyncBaseSocket(microsocket.BaseSocket):
+	def __init__(self):
+		self.socket = socket.socket()
+		self.socket.setblocking(False)
 
 
 class AsyncSelectWrapper(microsocket.SelectWrapper):
-
-    async def wait_until(self, method):
-        while not method():
-            await YIELD_TO_LOOP()
+	async def wait_until(self, method):
+		while not method():
+			await YIELD_TO_LOOP()
 
 
 class AsyncServer(AsyncBaseSocket, AsyncSelectWrapper, microsocket.Server):
-
-    async def accept(self):
-        """Accept a client and return it."""
-        await self.wait_until(self.isreadable)
-        return AsyncAcceptedClient(*self.socket.accept())
+	async def accept(self):
+		"""Accept a client and return it."""
+		await self.wait_until(self.isreadable)
+		return AsyncAcceptedClient(*self.socket.accept())
 
 
 class AsyncBaseClient(AsyncBaseSocket, AsyncSelectWrapper, microsocket.BaseClient):
+	async def _safe_send(self, msg):
+		totalsent = 0
+		while totalsent < len(msg):
+			sent = self.socket.send(msg[totalsent:])
+			if sent:
+				totalsent += sent
+			else:
+				raise ConnectionError("Socket connection is broken.")
+			await YIELD_TO_LOOP()
 
-    async def _safe_send(self, msg):
-        totalsent = 0
-        while totalsent < len(msg):
-            sent = self.socket.send(msg[totalsent:])
-            if sent:
-                totalsent += sent
-            else:
-                raise ConnectionError("Socket connection is broken.")
-            await YIELD_TO_LOOP()
+	async def _safe_recv(self, lenght, block=False):
+		return_value = b""
+		bytes_recd = 0
+		while bytes_recd < lenght:
+			await self.wait_until(self.isreadable)
+			chunk = self.socket.recv(lenght - bytes_recd)
+			if chunk:
+				return_value += chunk
+				bytes_recd += len(chunk)
+			else:
+				raise ConnectionError("Socket connection is broken.")
+		return return_value
 
-    async def _safe_recv(self, lenght, block = False):
-        return_value = b""
-        bytes_recd = 0
-        while bytes_recd < lenght:
-            await self.wait_until(self.isreadable)
-            chunk = self.socket.recv(lenght - bytes_recd)
-            if chunk:
-                return_value += chunk
-                bytes_recd += len(chunk)
-            else:
-                raise ConnectionError("Socket connection is broken.")
-        return return_value
+	async def send_obj(self, obj):
+		"""Send an object to the other socket."""
+		bytes_to_send = SERIALIZER(obj).encode()
+		await self._safe_send(_struct.pack(len(bytes_to_send)) + bytes_to_send)
 
-    async def send_obj(self, obj):
-        """Send an object to the other socket."""
-        bytes_to_send = SERIALIZER(obj).encode()
-        await self._safe_send(_struct.pack(len(bytes_to_send)) + bytes_to_send)
-
-    async def recv_obj(self, block = False):
-        """Receive an object from the other socket."""
-        lenght = _struct.unpack(self._safe_recv(_size))[0]
-        return DESERIALIZER(await self._safe_recv(lenght, block).decode())
+	async def recv_obj(self, block=False):
+		"""Receive an object from the other socket."""
+		lenght = _struct.unpack(self._safe_recv(_size))[0]
+		return DESERIALIZER(await self._safe_recv(lenght, block).decode())
 
 
 class AsyncAcceptedClient(AsyncBaseClient, microsocket.AcceptedClient):
-    pass
+	pass
 
 
 class AsyncClient(AsyncBaseClient, microsocket.Client):
-    """Client class for connecting to the server."""
+	"""Client class for connecting to the server."""
 
-    async def connect(self, address):
-        """Connect to the server."""
-        while True:
-            try:
-                self.socket.connect(address) # use settimeout instead?
-            except BlockingIOError:
-                pass
-            await YIELD_TO_LOOP()
+	async def connect(self, address):
+		"""Connect to the server."""
+		while True:
+			try:
+				self.socket.connect(address)  # use settimeout instead?
+			except BlockingIOError:
+				pass
+			await YIELD_TO_LOOP()
 
 
 if __name__ == "__main__":
-    async def display():
-        anim = "|/-\\"
-        x = 0
-        while True:
-            print(f"\r[{anim[x]}]", end = "")
-            x += 1
-            x %= len(anim)
-            await asyncio.sleep(0.25)
 
-    async def main():
-        asyncio.create_task(display())
-        await AsyncClient().connect(("", 8000))
+	async def display():
+		anim = "|/-\\"
+		x = 0
+		while True:
+			print(f"\r[{anim[x]}]", end="")
+			x += 1
+			x %= len(anim)
+			await asyncio.sleep(0.25)
 
-    asyncio.run(main())
+	async def main():
+		asyncio.create_task(display())
+		await AsyncClient().connect(("", 8000))
+
+	asyncio.run(main())
--- chat.py
+++ chat.py
@@ -3,11 +3,12 @@ from time import sleep
 
 PORT = 3030
 
+
 def main():
 	name = input("Adınız:\n > ")
-	
+
 	client = [True, False][int(input("Başkasına bağlanmak için 1, bağlantı beklemek için 2'ye basın.\n > ")) - 1]
-	
+
 	if client:
 		listener = BroadcastListener()
 
@@ -25,7 +26,6 @@ def main():
 		conn = Client()
 		conn.connect((ip, PORT))
 	else:
-
 		print(f"Bağlanacak cihazlar bekleniyor (bu cihazın adresi: {LAN}).")
 		conn = Server()
 
@@ -53,6 +53,7 @@ def main():
 		else:
 			print(f"{other_name}: {conn.recv_obj()}")
 		turn = not turn
+
 
 if __name__ == "__main__":
 	try:
